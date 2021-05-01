e5c410f07d6766e8ed865c747d8112536bb8c9f3
Ignacio Hagopian <jsign.uy@gmail.com>
2021-05-01 19:09:16 -0300

    add bls & fix secp bug
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- signer.go
+++ signer.go
@@ -6,10 +6,11 @@ import (
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/dchest/blake2b"
 	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/phoreproject/bls/g1pubs"
 )
 
 // Pure-go secp256k1 signer.
-// Main problem with this is that Lotus encodes signature
+// Main problem with this is that Lotus relies on signatures
 // with the R || S || V format. We don't have V here.
 func SignSecp256k1(pk []byte, msg []byte) (crypto.Signature, error) {
 	priv, _ := btcec.PrivKeyFromBytes(btcec.S256(), pk)
@@ -20,17 +21,30 @@ func SignSecp256k1(pk []byte, msg []byte) (crypto.Signature, error) {
 	}
 	return crypto.Signature{
 		Type: crypto.SigTypeSecp256k1,
-		Data: serializeSig(sig),
+		Data: serializeSecp256k1Signature(sig),
 	}, nil
 }
 
-// TODO: byte65 should be fixed. it's V for self validating
-// signature.
-func serializeSig(sig *btcec.Signature) []byte {
-	rBytes := sig.R.Bytes()
-	sBytes := sig.S.Bytes()
+func serializeSecp256k1Signature(sig *btcec.Signature) []byte {
 	sigBytes := make([]byte, 65)
+	rBytes := sig.R.Bytes()
 	copy(sigBytes[32-len(rBytes):32], rBytes)
+	sBytes := sig.S.Bytes()
 	copy(sigBytes[64-len(sBytes):64], sBytes)
+	sigBytes[64] = 0 // TODO: V?
 	return sigBytes
+}
+
+func SignBLS(pk []byte, msg []byte) (crypto.Signature, error) {
+	var apk [32]byte
+	copy(apk[:], pk)
+
+	sk := g1pubs.DeriveSecretKey(apk)
+	sig := g1pubs.Sign(msg, sk)
+	sigRaw := sig.Serialize()
+
+	return crypto.Signature{
+		Type: crypto.SigTypeBLS,
+		Data: sigRaw[:],
+	}, nil
 }
--- signer_test.go
+++ signer_test.go
@@ -3,23 +3,38 @@ package signer
 import (
 	"encoding/base64"
 	"encoding/hex"
-	"fmt"
 	"testing"
 
+	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/stretchr/testify/require"
 )
 
 func TestSecp256k1Sign(t *testing.T) {
-	pkBase64 := "k5Pyv3qH2sIXcCYZXYOWuE1I2n2UE9ChaUklN6iZWcE="
-	pk, err := base64.StdEncoding.DecodeString(pkBase64)
-	require.NoError(t, err)
-	msg := []byte("DUKEDUKEDUKEDUKE")
+	pk64 := "k5Pyv3qH2sIXcCYZXYOWuE1I2n2UE9ChaUklN6iZWcE="
+	lotusFormatSig := testSign(t, pk64, SignSecp256k1)
 
-	sig, err := SignSecp256k1(pk, msg)
+	// lotus wallet sign f1fib3pv7jua2ockdugtz7viz3cyy6lkhh7rfx3sa 44554b45
+	require.Equal(t, "0103bff286f1371c1a4ce8e33c29d6a20eeb53f17970190d12c5a1c0fc4be9a56e766250d5a82dda2179fa90ae297696d1dfaa9eea8f2f833da0cf87b927294eb700", lotusFormatSig)
+}
+
+func TestBLSSign(t *testing.T) {
+	pk64 := "hbp/yFfRt9QLCqkmVaqAWRCoPUgw1KwiqqnshNIpNWQ="
+	lotusFormatSig := testSign(t, pk64, SignBLS)
+
+	// lotus wallet sign f3rpskqryflc2sqzzzu7j2q6fecrkdkv4p2avpf4kyk5u754he7g6cr2rbpmif7pam5oxbme2oyzot4ry3d74q 44554b45
+	require.Equal(t, "0280cef41af956cf6855725a5af0d8d956ea41f6da2688cc1f436a7ea5ebd15d96ba384746b819c0836c53a54d6d30b0a70b528204e9da92f833031e7a4776f57bad83e033ea0235bad0f8a5337a4eb66870fc0429175ed63f91203ec1b65c6e31", lotusFormatSig)
+}
+
+func testSign(t *testing.T, pk64 string, sch func(pk []byte, msg []byte) (crypto.Signature, error)) string {
+	msg := []byte("DUKE")
+
+	pk, err := base64.StdEncoding.DecodeString(pk64)
+	require.NoError(t, err)
+
+	sig, err := sch(pk, msg)
 	require.NoError(t, err)
 
 	sigm, err := sig.MarshalBinary()
 	require.NoError(t, err)
-	fmt.Printf("Message: %s\n", hex.EncodeToString(msg))
-	fmt.Printf("Signed message: %s\n", hex.EncodeToString(sigm))
+	return hex.EncodeToString(sigm)
 }
