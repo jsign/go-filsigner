c10fcdef068f41ba07ac39c7f4e115d7d6fa14c5
Ignacio Hagopian <jsign.uy@gmail.com>
2021-05-08 14:00:12 -0300

    secp256k1 fully working
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/jsign/go-filsigner
 go 1.16
 
 require (
-	github.com/btcsuite/btcd v0.21.0-beta
 	github.com/dchest/blake2b v1.0.0
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.0-20210507181900-4e0be8d2fbb4
 	github.com/filecoin-project/go-state-types v0.1.0
 	github.com/filecoin-project/lotus v1.8.0
 	github.com/jsign/bls v0.0.0-20210505222336-ece047ccd126
--- signer.go
+++ signer.go
@@ -4,8 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/btcsuite/btcd/btcec"
 	"github.com/dchest/blake2b"
+	"github.com/decred/dcrd/dcrec/secp256k1/v4"
+	"github.com/decred/dcrd/dcrec/secp256k1/v4/ecdsa"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/jsign/bls/g1pubs"
@@ -13,6 +14,9 @@ import (
 	"encoding/hex"
 )
 
+// Sign generates a signature for a Filecoin public address.
+// Both Secp256k1 and BLS private keys can be used. Private keys are expected
+// to be
 func Sign(lotusExportedPK string, msg []byte) (*crypto.Signature, error) {
 	ki, err := decodeLotusExportedPK(lotusExportedPK)
 	if err != nil {
@@ -29,23 +33,20 @@ func Sign(lotusExportedPK string, msg []byte) (*crypto.Signature, error) {
 	}
 }
 
-// Pure-go secp256k1 signer!
-// This has a problem. The serialization format used in `filecoin-project/go-crypto`,
-// is R||S||V to allow for self-verifying signatures. But btcec doesn't
-// return V. For now it appends always V=0, but V can be 0 or 1, this should
-// be calculated correctly.
-// Lotus uses `filecoin-project/go-crypto` which ends up using
-// https://github.com/ipsn/go-secp256k1/blob/9d62b9f0bc52d16160f79bfb84b2bbf0f6276b03/secp256.go#L53
 func signSecp256k1(pk []byte, msg []byte) (*crypto.Signature, error) {
-	priv, _ := btcec.PrivKeyFromBytes(btcec.S256(), pk)
+	priv := secp256k1.PrivKeyFromBytes(pk)
 	msgHash := blake2b.Sum256(msg)
-	sig, err := priv.Sign(msgHash[:])
-	if err != nil {
-		return nil, fmt.Errorf("signing: %s", err)
-	}
+	sig := ecdsa.SignCompact(priv, msgHash[:], false)
+
+	// We need to left-rotate by 1 byte, and adjust the
+	// recovery ID to be zero-centered.
+	recoveryID := sig[0]
+	copy(sig, sig[1:])
+	sig[64] = recoveryID - 27
+
 	return &crypto.Signature{
 		Type: crypto.SigTypeSecp256k1,
-		Data: serializeSecp256k1Signature(sig),
+		Data: sig,
 	}, nil
 }
 
@@ -65,16 +66,6 @@ func signBLS(pk []byte, msg []byte) (*crypto.Signature, error) {
 		Type: crypto.SigTypeBLS,
 		Data: sigRaw[:],
 	}, nil
-}
-
-func serializeSecp256k1Signature(sig *btcec.Signature) []byte {
-	sigBytes := make([]byte, 65)
-	rBytes := sig.R.Bytes()
-	copy(sigBytes[32-len(rBytes):32], rBytes)
-	sBytes := sig.S.Bytes()
-	copy(sigBytes[64-len(sBytes):64], sBytes)
-	sigBytes[64] = 0 // TODO: calculate correct V? see secp256k1 method comment.
-	return sigBytes
 }
 
 func decodeLotusExportedPK(lotusExportedPK string) (*types.KeyInfo, error) {
