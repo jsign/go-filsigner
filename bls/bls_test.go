18f8e901d4fbb466fcb681b1e79ff1e8fac39f5a
Ignacio Hagopian <jsign.uy@gmail.com>
2021-05-15 22:33:06 -0300

    bls/secp256k1: basic bench
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

---
+++ bls/bls_test.go
@@ -0,0 +1,25 @@
+package bls
+
+import (
+	"crypto/rand"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var sink []byte
+
+func BenchmarkSign(b *testing.B) {
+	pk := make([]byte, 32)
+	_, err := io.ReadFull(rand.Reader, pk)
+	require.NoError(b, err)
+
+	msg := []byte("duke+jsign")
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sink, _ = Sign(pk, msg)
+	}
+}
